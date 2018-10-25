3af3ff63364319f79a13046bc89bf3c095931c1c
Steven VanWoerkom <vanwoerkom.steve@gmail.com>
2018-10-24 20:15:49 -0400

    starter code

---
+++ db/records.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    id: 1,
+    artist: 'Nosaj Thing',
+    album: 'Parallels',
+    year: 2017
+  },
+  {
+    id: 2,
+    artist: 'Four Tet',
+    album: 'New Energy',
+    year: 2017
+  },
+  {
+    id: 3,
+    artist: 'DJ Koze',
+    album: 'Knock Knock',
+    year: 2018
+  },
+  {
+    id: 4,
+    artist: 'Mount Kimbie',
+    album: 'Love What Survives',
+    year: 2017
+  },
+  {
+    id: 5,
+    artist: 'Ariel Pink',
+    album: 'Dedicated to Bobby Jameson',
+    year: 2017
+  },
+  {
+    id: 6,
+    artist: 'SZA',
+    album: 'CTRL',
+    year: 2017
+  }
+]
