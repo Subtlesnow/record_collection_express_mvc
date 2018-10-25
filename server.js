392846602175dd108fa78bc1df27b4862ea1595e
Steven VanWoerkom <vanwoerkom.steve@gmail.com>
2018-10-24 20:39:38 -0400

    dependencies and middleware

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-// import express from our dependencies
-
-// initialize app
-
+// import dependencies and initialize app
+const express = require('express')
+const app = express()
+const port = 3000
+const logger = require('morgan')
+const bodyParser = require('body-parser')
 // middleware
-
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 // index route
 
 // add routes
 
 // add error handler
+app.listen(port, () => console.log(`Rock'n on port ${port}`))
